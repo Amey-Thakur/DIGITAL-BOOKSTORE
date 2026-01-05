8570e54b54bda27200b5060e991cceb2d4165b2e
Amey Thakur <ameythakur20@gmail.com>
2026-01-04 22:22:23 -0500

    Digital Bookstore

--- Source Code/register.php
+++ Source Code/register.php
@@ -2,7 +2,9 @@
 /**
  * DIGITAL-BOOKSTORE
  *
- * User registration management module.
+ * User enrollment and identity provisioning module.
+ * Facilitates the creation of unique user identifiers and the storage 
+ * of secure credentials within the persistence layer.
  *
  * @category   Full Stack Web Application
  * @package    Digital Bookstore Management System
@@ -15,21 +17,33 @@
 
 include "dbconnect.php";
 
-
+/**
+ * Identity Lifecycle Handler
+ * Manages the allocation and verification of new user identities.
+ */
 if (isset($_POST['submit'])) {
   if ($_POST['submit'] == "register") {
-    $username = $_POST['register_username'];
-    $password = $_POST['register_password'];
-    $query = "select * from users where UserName = '$username'";
-    $result = mysqli_query($con, $query) or die(mysql_error);
-    if (mysqli_num_rows($result) > 0) {
-      header("Location: index.php?register=" . "Username is already taken...Use different username");
+    /**
+     * Sanitization & Conflict Detection
+     * Verifies the uniqueness of the requested identifier in the global repository.
+     */
+    $username = mysqli_real_escape_string($con, $_POST['register_username']);
+    $password = mysqli_real_escape_string($con, $_POST['register_password']);
+
+    $check_query = "SELECT * FROM users WHERE UserName = '$username'";
+    $check_result = mysqli_query($con, $check_query) or die(mysqli_error($con));
+
+    if (mysqli_num_rows($check_result) > 0) {
+      header("Location: index.php?Message=" . urlencode("Identification Conflict: Username is already allocated. Use a different identifier."));
     } else {
-      $query = "INSERT INTO users VALUES ('$username','$password')";
-      $result = mysqli_query($con, $query);
-      header("Location: index.php?register=" . "Successfully Registered!!");
+      /**
+       * Resource Persistence
+       * Commits the new identity credentials to the 'users' entity.
+       */
+      $insert_query = "INSERT INTO users (UserName, Password) VALUES ('$username', '$password')";
+      $insert_result = mysqli_query($con, $insert_query) or die(mysqli_error($con));
+      header("Location: index.php?Message=" . urlencode("Enrollment Successful. You may now proceed to authenticate."));
     }
   }
 }
-
 ?>
