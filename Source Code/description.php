2d0b53e18f173b212191eac53746e5f2fc273e1e
Amey Thakur <ameythakur20@gmail.com>
2026-01-04 22:19:58 -0500

    Digital Bookstore

--- Source Code/description.php
+++ Source Code/description.php
@@ -4,6 +4,7 @@
  *
  * Detailed product metadata presentation layer.
  * Renders exhaustive book specifications and handles cart-entry preparations.
+ * Facilitates hierarchical data visualization for library artifacts.
  *
  * @category   Full Stack Web Application
  * @package    Digital Bookstore Management System
@@ -29,8 +30,9 @@ if (!isset($_SESSION['user']))
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Books">
   <meta name="author" content="Amey Thakur">
-  <title> Digital Bookstore</title>
-  <!-- Bootstrap -->
+  <title>Book Description | Digital Bookstore</title>
+
+  <!-- UI Framework Dependencies -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <link href="css/my.css" rel="stylesheet">
 
@@ -101,6 +103,7 @@ if (!isset($_SESSION['user']))
 </head>
 
 <body>
+  <!-- Top Navigation Bar -->
   <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
     <div class="container-fluid">
       <!-- Brand and toggle get grouped for better mobile display -->
@@ -112,14 +115,19 @@ if (!isset($_SESSION['user']))
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.php"><img alt="Brand" src="img/logo.jpg"
-            style="width: 118px;margin-top: -7px;margin-left: -10px;"></a>
+        <a class="navbar-brand" href="index.php">
+          <img alt="Brand" src="img/logo.jpg" style="width: 118px;margin-top: -7px;margin-left: -10px;">
+        </a>
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav navbar-right">
           <?php
+          /**
+           * Session-Specific Navigation
+           * Renders authenticated user options.
+           */
           if (isset($_SESSION['user'])) {
             echo '
                     <li><a href="cart.php" class="btn btn-md"><span class="glyphicon glyphicon-shopping-cart">Cart</span></a></li>
@@ -132,6 +140,7 @@ if (!isset($_SESSION['user']))
     </div><!-- /.container-fluid -->
   </nav>
 
+  <!-- Search Interface Section -->
   <div id="top">
     <div id="searchbox" class="container-fluid" style="width:112%;margin-left:-6%;margin-right:-6%;">
       <div>
@@ -145,105 +154,105 @@ if (!isset($_SESSION['user']))
 
 
   <?php
+  /**
+   * Data Extraction Engine
+   * Retrieves comprehensive artifact metadata using the Primary Identifier (PID).
+   */
   include "dbconnect.php";
-  $PID = $_GET['ID'];
-  $query = "SELECT * FROM products WHERE PID='$PID'";
-  $result = mysqli_query($con, $query) or die(mysql_error());
+  if (isset($_GET['ID'])) {
+    $PID = mysqli_real_escape_string($con, $_GET['ID']);
+    $query = "SELECT * FROM products WHERE PID='$PID'";
+    $result = mysqli_query($con, $query) or die(mysqli_error($con));
 
-  if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-      $path = "img/books/" . $row['PID'] . ".jpg";
-      $target = "cart.php?ID=" . $PID . "&";
-      echo '
-  <div class="container-fluid" id="books">
-    <div class="row">
-      <div class="col-sm-10 col-md-6">
-                          <div class="tag">' . $row["Discount"] . '%OFF</div>
-                              <div class="tag-side"><img src="img/orange-flag.png">
-                          </div>
-                         <img class="center-block img-responsive" src="' . $path . '" height="550px" style="padding:20px;">
-      </div>
-      <div class="col-sm-10 col-md-4 col-md-offset-1">
-        <h2> ' . $row["Title"] . '</h2>
-                                <span style="color:#00B9F5;">
-                                 #' . $row["Author"] . '&nbsp &nbsp #' . $row["Publisher"] . '
-                                </span>
-        <hr>
-                                <span style="font-weight:bold;"> Quantity : </span>';
-      echo '<select id="quantity">';
-      for ($i = 1; $i <= $row['Available']; $i++)
-        echo '<option value="' . $i . '">' . $i . '</option>';
-      echo ' </select>';
-      echo '                           <br><br><br>
-                                <a id="buyLink" href="' . $target . '" class="btn btn-lg btn-danger" style="padding:15px;color:white;text-decoration:none;">
-                                    ADD TO CART for Rs ' . $row["Price"] . ' <br>
-                                    <span style="text-decoration:line-through;"> RS' . $row["MRP"] . '</span>
-                                    | ' . $row["Discount"] . '% discount
-                                 </a>
+    if (mysqli_num_rows($result) > 0) {
+      while ($row = mysqli_fetch_assoc($result)) {
+        $path = "img/books/" . $row['PID'] . ".jpg";
+        $target = "cart.php?ID=" . $PID . "&";
+        echo '
+    <div class="container-fluid" id="books">
+        <div class="row">
+            <div class="col-sm-10 col-md-6">
+                <div class="tag">' . $row["Discount"] . '% OFF</div>
+                <div class="tag-side"><img src="img/orange-flag.png"></div>
+                <img class="center-block img-responsive" src="' . $path . '" style="padding:20px; max-height:550px; border-radius:10px;">
+            </div>
+            <div class="col-sm-10 col-md-4 col-md-offset-1">
+                <h2>' . $row["Title"] . '</h2>
+                <span style="color:#00B9F5;">
+                    #' . $row["Author"] . '&nbsp;&nbsp;#' . $row["Publisher"] . '
+                </span>
+                <hr>
+                <span style="font-weight:bold;">Quantity:</span>';
 
-      </div>
-    </div>
-          </div>
-     ';
-      echo '
-          <div class="container-fluid" id="description">
-    <div class="row">
-      <h2> Description </h2>
-                        <p>' . $row['Description'] . '</p>
-                        <pre style="background:inherit;border:none;">
-   PRODUCT CODE  ' . $row["PID"] . '   <hr>
-   TITLE         ' . $row["Title"] . ' <hr>
-   AUTHOR        ' . $row["Author"] . ' <hr>
-   AVAILABLE     ' . $row["Available"] . ' <hr>
-   PUBLISHER     ' . $row["Publisher"] . ' <hr>
-   EDITION       ' . $row["Edition"] . ' <hr>
-   LANGUAGE      ' . $row["Language"] . ' <hr>
-   PAGES         ' . $row["page"] . ' <hr>
-   WEIGHT        ' . $row["weight"] . ' <hr>
-                        </pre>
-    </div>
-  </div>
-';
+        echo '<select id="quantity">';
+        for ($i = 1; $i <= $row['Available']; $i++)
+          echo '<option value="' . $i . '">' . $i . '</option>';
+        echo ' </select>';
 
+        echo ' <br><br><br>
+                <a id="buyLink" href="' . $target . '" class="btn btn-lg btn-danger" style="padding:15px; color:white; text-decoration:none; font-weight:800; border-radius:5px;">
+                    ADD TO CART for INR ' . $row["Price"] . ' <br>
+                    <span style="text-decoration:line-through;">RS ' . $row["MRP"] . '</span>
+                    | ' . $row["Discount"] . '% SAVINGS
+                </a>
+            </div>
+        </div>
+    </div>';
 
+        echo '
+    <div class="container-fluid" id="description">
+        <div class="row">
+            <h2 style="color:#D67B22;">Technical Specifications</h2>
+            <p>' . $row['Description'] . '</p>
+            <pre style="background:inherit; border:none; color:#555;">
+PRODUCT CODE  ' . $row["PID"] . '   <hr>
+TITLE         ' . $row["Title"] . ' <hr>
+AUTHOR        ' . $row["Author"] . ' <hr>
+AVAILABLE     ' . $row["Available"] . ' <hr>
+PUBLISHER     ' . $row["Publisher"] . ' <hr>
+EDITION       ' . $row["Edition"] . ' <hr>
+LANGUAGE      ' . $row["Language"] . ' <hr>
+PAGES         ' . $row["page"] . ' <hr>
+WEIGHT        ' . $row["weight"] . ' <hr>
+            </pre>
+        </div>
+    </div>';
+      }
     }
   }
-  echo '</div>';
   ?>
 
 
-
+  <!-- Service Value Propositions -->
   <div class="container-fluid" id="service">
     <div class="row">
       <div class="col-sm-6 col-md-3 text-center">
         <span class="glyphicon glyphicon-heart"></span> <br>
-        24X7 Care <br>
-        Happy to help 24X7, call us on 0120-3062244 or click here
+        <strong>24X7 Care</strong> <br>
+        Happy to help 24X7, call us on 0120-3062244.
       </div>
       <div class="col-sm-6 col-md-3 text-center">
         <span class="glyphicon glyphicon-ok"></span> <br>
-        Trust <br>
-        Your money is yours! All refunds come with no question asked guarantee.
+        <strong>Trust</strong> <br>
+        Your security is paramount. All transactions are encrypted.
       </div>
       <div class="col-sm-6 col-md-3 text-center">
         <span class="glyphicon glyphicon-check"></span> <br>
-        Assurance <br>
-        We provide 100% assurance. If you have any issue, your money is immediately refunded. Sit back and enjoy your
-        shopping.
+        <strong>Assurance</strong> <br>
+        100% genuine academic artifacts guaranteed.
       </div>
       <div class="col-sm-6 col-md-3 text-center">
         <span class="glyphicon glyphicon-tags"></span> <br>
-        24X7 Care <br>
-        Happiness is guaranteed. If we fall short of your expectations, give us a shout.
+        <strong>Value</strong> <br>
+        Best price guarantee across the digital library ecosystem.
       </div>
     </div>
   </div>
 
 
 
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <!-- Script Dependencies -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <!-- Include all compiled plugins (below), or include individual files as needed -->
   <script src="js/bootstrap.min.js"></script>
   <script>
     /**
